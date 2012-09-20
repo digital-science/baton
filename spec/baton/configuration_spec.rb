12d92b16a430c395733ea7039817ac44b89afe5a
Carlos Vilhena <carlosvilhena@gmail.com>
2012-09-20 14:15:40 +0100

    delete training spaces

--- spec/baton/configuration_spec.rb
+++ spec/baton/configuration_spec.rb
@@ -7,24 +7,24 @@ describe Baton::Configuration do
       before(:each) do
         subject.config_path = "#{File.dirname(__FILE__)}/../fixtures/config.cfg"
       end
-      
+
       it "will set the host" do
         subject.host.should eq("fromconfig.com")
       end
-      
+
       it "will set the vhost" do
         subject.vhost.should eq("fromconfig")
       end
-      
+
       it "will set the user" do
         subject.user.should eq("fromconfiguser")
       end
-      
+
       it "will set the password" do
         subject.password.should eq("fromconfigpass")
       end
     end
-    
+
     context "given a non existing file" do
       it "will log an erorr" do
         subject.logger.should_receive(:error).with("Could not find a baton configuration file at bad_path")
@@ -32,22 +32,22 @@ describe Baton::Configuration do
       end
     end
   end
-  
+
   describe "#connection_opts" do
     before(:each) do
       subject.config_path = "#{File.dirname(__FILE__)}/../fixtures/config.cfg"
     end
-    
+
     context "give a config file" do
       it "will return a config hash" do
         subject.connection_opts.should eq({:host=>"fromconfig.com", :vhost=>"fromconfig", :user=>"fromconfiguser", :password=>"fromconfigpass", :pass=>"fromconfigpass"})
       end
     end
-    
+
     context "given one of the configuration options is nil" do
       it "will not be returned in the config hash" do
         subject.vhost = nil
-        subject.connection_opts.should eq({:host=>"fromconfig.com", :user=>"fromconfiguser", :password=>"fromconfigpass", :pass=>"fromconfigpass"})        
+        subject.connection_opts.should eq({:host=>"fromconfig.com", :user=>"fromconfiguser", :password=>"fromconfigpass", :pass=>"fromconfigpass"})
       end
     end
   end
