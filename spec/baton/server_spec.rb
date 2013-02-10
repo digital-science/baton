f52da777e9aea6b9d890930f750ca9e1d0ac8712
William Roe <willroe@gmail.com>
2013-02-10 19:58:09 +0000

    New test for integration with ohai plugins
    This test will fail if the chef_environment plugin cannot be loaded by
    Ohai - the situation that was happening with the non-pinned chef gem

--- spec/baton/server_spec.rb
+++ spec/baton/server_spec.rb
@@ -2,57 +2,74 @@ require "spec_helper"
 require "baton/server"
 
 describe Baton::Server do
-  before(:each) do
-    Ohai::System.any_instance.stub(:all_plugins).and_return(true)
-    Ohai::System.any_instance.stub(:data).and_return({
-      "chef_environment" => "production",
-      "fqdn" => "build-prod-i-722b0004.dsci.it",
-      "trebuchet" => ["octobutler"]
-    })
-  end
+  context "stubbed ohai" do
+    before(:each) do
+      Ohai::System.any_instance.stub(:all_plugins).and_return(true)
+      Ohai::System.any_instance.stub(:data).and_return({
+        "chef_environment" => "production",
+        "fqdn" => "build-prod-i-722b0004.dsci.it",
+        "trebuchet" => ["octobutler"]
+      })
+    end
 
-  describe "#configure" do
-    context "given data from Ohai" do
+    describe "#configure" do
+      context "given data from Ohai" do
 
-      it "will set the fqdn" do
-        subject.fqdn.should eq("build-prod-i-722b0004.dsci.it")
+        it "will set the fqdn" do
+          subject.fqdn.should eq("build-prod-i-722b0004.dsci.it")
+        end
+
+        it "will set the environment" do
+          subject.environment.should eq("production")
+        end
+
+        it "will set the apps" do
+          subject.app_names.first.should eq("octobutler")
+        end
       end
 
-      it "will set the environment" do
-        subject.environment.should eq("production")
-      end
+      context "given the required facts are not available" do
+        before(:each) do
+          Baton::Server.any_instance.stub(:facts).and_return({})
+          subject.configure
+        end
 
-      it "will set the apps" do
-        subject.app_names.first.should eq("octobutler")
+        it "will default the fqdn to an empty string" do
+          subject.fqdn.should be_empty
+        end
+
+        it "will default environment to development" do
+          subject.environment.should eq("development")
+        end
+
+        it "will default apps to an empty array" do
+          subject.app_names.should be_empty
+        end
       end
     end
 
-    context "given the required facts are not available" do
-      before(:each) do
-        Baton::Server.any_instance.stub(:facts).and_return({})
-        subject.configure
-      end
-
-      it "will default the fqdn to an empty string" do
-        subject.fqdn.should be_empty
-      end
-
-      it "will default environment to development" do
-        subject.environment.should eq("development")
-      end
-
-      it "will default apps to an empty array" do
-        subject.app_names.should be_empty
+    describe "#attributes" do
+      context "given an instance of a server" do
+        it "should have the attributes set" do
+          subject.attributes.should eq({:environment=>"production",
+                                        :fqdn=>"build-prod-i-722b0004.dsci.it",
+                                        :app_names=>["octobutler"]})
+        end
       end
     end
   end
 
-  describe "#attributes" do
-    context "given an instance of a server" do
-      it "should have the attributes set" do
-        subject.attributes.should eq({:environment=>"production",
-          :fqdn=>"build-prod-i-722b0004.dsci.it",
-          :app_names=>["octobutler"]})
+  context "ohai integration" do
+    before(:each) do
+      ohai_fixtures = File.expand_path('../../fixtures/ohai_plugins', __FILE__)
+      Baton::Server.any_instance.stub(:ohai_plugin_path).and_return(ohai_fixtures)
+    end
+
+    describe "#environment" do
+      context "production" do
+        it "should have the correct environment set" do
+          subject.environment.should eq('production')
+        end
       end
     end
   end
---
+++ spec/fixtures/etc/chef/client.rb
@@ -0,0 +1,9 @@
+log_level          :info
+log_location       STDOUT
+ssl_verify_mode    :verify_none
+file_cache_path    "/var/cache/chef"
+file_backup_path   "/var/lib/chef/backup"
+pid_file           "/var/run/chef/client.pid"
+os                 "FakeOS"
+environment        "production"
+
