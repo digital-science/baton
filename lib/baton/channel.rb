8cdf0f360de4358ce8e470127d5a8d7afb05947c
Patrick Steward <psteward@expedia.com>
2015-09-14 10:37:33 -0700

    TCP failures no longer spam the logs - the TCP connection failure callback has been improved with a configurable time between reconnect attempts.

--- lib/baton/channel.rb
+++ lib/baton/channel.rb
@@ -5,18 +5,20 @@ module Baton
   class Channel
     include Baton::Logging
 
-    attr_accessor :channel, :exchange_in, :exchange_out, :connection, :connection_options, :amqp_hosts
+    attr_accessor :channel, :exchange_in, :exchange_out, :connection, :connection_options, :amqp_hosts, :last_reconnect_time
 
-    # Public: Initialize a Channel. It creates an AMQP  connection, a channel,
+    # Public: Initialize a Channel. It creates an AMQP connection, a channel,
     # an input and an output exchange and finally attaches the handle_channel_exception
     # callback to the on_error event on the channel.
     def initialize
-
+      
       @connection_options = Baton.configuration.connection_opts
       @amqp_hosts = Baton.configuration.amqp_host_list
 
-      logger.info "Connecting to AMQP host: #{@connection_options[:host]}"
-
+      logger.info "Connecting to AMQP host: #{@connection_options[:host]}:#{@connection_options[:port]}"
+       
+      @reconnect_wait = 5
+      @last_reconnect_time = Time.new
       @connection   = AMQP.connect(@connection_options)
       @channel      = AMQP::Channel.new(@connection)
       @channel.auto_recovery = true
@@ -37,8 +39,9 @@ module Baton
       end
 
       # Attach callbacks for error handling
-      @connection.on_tcp_connection_loss(&method(:handle_tcp_failure))
-      @connection.on_skipped_heartbeats(&method(:handle_tcp_failure))
+      @connection.on_tcp_connection_loss(&method(:handle_tcp_loss))
+      @connection.on_tcp_connection_failure(&method(:handle_tcp_failure))
+      @connection.on_possible_authentication_failure(&method(:handle_authentication_failure))
       @channel.on_error(&method(:handle_channel_exception))
     end
 
@@ -86,13 +89,19 @@ module Baton
     #
     # Returns nothing.
     #
-    def handle_tcp_failure(connection, settings)
+    def handle_tcp_loss(connection, settings)
 
-      logger.info("Connection to AMQP lost. Finding new host..")
+      # if we tried to reconnect within the last @reconnect_wait seconds, do nothing
+      if @last_reconnect_time > Time.new - @reconnect_wait
+	return
+      end
+
+      logger.info("Connection to AMQP host lost. Finding new host...")
 
       if @amqp_hosts.size == 1
-        logger.info("Only a single host.. reconnecting")
-        connection.reconnect(false, 10)
+        logger.info("Only a single host (#{settings[:host]}); reconnecting...")
+	@last_reconnect_time = Time.new
+        connection.reconnect(true)
         return
       end
 
@@ -105,9 +114,23 @@ module Baton
 
       settings[:host] = new_host
 
-      logger.info ("Reconnecting to AMPQ host: #{new_host}")
+      logger.info("Connecting to new AMQP host: #{new_host}")
 
       connection.reconnect_to(settings)
+
+      @last_reconnect_time = Time.new
+    end
+
+    def handle_tcp_failure(settings)
+      logger.error("Initial connection to server #{settings[:host]}:#{settings[:port]} failed");
+
+      exit 1
+    end
+
+    def handle_authentication_failure(settings)
+      logger.error("Authentication failed with settings:\n#{settings.inspect}");
+
+      exit 1
     end
   end
 end
